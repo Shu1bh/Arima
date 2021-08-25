7ee32d33de6aec40b87bd71bb64f3d1498024635
Shu1bh <sagnihotri32@gmail.com>
2021-08-25 16:47:44 +0530

    Update predict_stock_price.py

--- predict_stock_price.py
+++ predict_stock_price.py
@@ -13,14 +13,14 @@ def begin():
     # load pickled logistic regression model
     arima_model = pickle.load(open("Arima_model.pickle", "rb"))
 
-def action(n):
+def action():
     global df_forecast, df_conf
     
-    forecast,conf_int = arima_model.predict(n_periods=n,return_conf_int=True)
+    forecast,conf_int = arima_model.predict(n_periods=30,return_conf_int=True)
     df_forecast = pd.DataFrame(forecast,columns=['close_pred'])
     df_conf = pd.DataFrame(conf_int,columns= ['Upper_bound','Lower_bound'])
     
-    yield df_forecast.to_dict(orient="records"),df_conf.to_dict(orient="records")
+    yield [df_forecast.to_dict(orient="records"),df_conf.to_dict(orient="records")]
 
 def metric(test):
     # Turn data into DataFrame
